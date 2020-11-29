898809a96efddd8b1b2f73106356ab69dbee36e2
unknown <reybernard1511@gmail.com>
2020-11-29 15:41:50 +0800

    dashboard_view

--- app/src/main/java/id/ac/cobalogin/DashboardActivity.java
+++ app/src/main/java/id/ac/cobalogin/DashboardActivity.java
@@ -1,7 +1,11 @@
 package id.ac.cobalogin;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -9,6 +13,7 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import androidx.annotation.Nullable;
@@ -18,13 +23,14 @@ public class DashboardActivity extends AppCompatActivity {
     private Button btnLogout;
     private ProgressDialog dialog;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
+        String name = getSharedPreferences(getBaseContext())).getString("name");
         TextView user = findViewById(R.id.loggedUser);
-//        user.setText();
+        user.setText(name);
 
         dialog = new ProgressDialog(getBaseContext());
         dialog.setCancelable(false);
@@ -41,11 +47,16 @@ public class DashboardActivity extends AppCompatActivity {
             try {
                 JSONObject object = new JSONObject(response);
                 if(object.getBoolean("success")){
-
+                    Preferences.clearLoggedUser(getApplicationContext());
+                    Toast.makeText(getApplicationContext(),"Log Out Success",Toast.LENGTH_SHORT).show();
                 }
-            }catch (){
-
+            }catch (JSONException e){
+                e.printStackTrace();
             }
+        }, error -> {
+            //error if fail to connect
+            error.printStackTrace();
+            dialog.dismiss();
         });
     }
 }
--- app/src/main/java/id/ac/cobalogin/Preferences.java
+++ app/src/main/java/id/ac/cobalogin/Preferences.java
@@ -1,5 +1,6 @@
 package id.ac.cobalogin;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
